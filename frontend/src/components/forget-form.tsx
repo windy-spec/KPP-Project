ee8a8857342f931b6d1bc383915d3efb8df6cf1f
ĐỖ THANH PHONG <phongdz072@gmail.com>
2025-10-24 23:35:45 +0700

    ket noi BE VA FE

--- frontend/src/components/forget-form.tsx
+++ frontend/src/components/forget-form.tsx
@@ -1,32 +1,57 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "./ui/label"
-import {z} from "zod";
-import {useForm} from 'react-hook-form';
-import {zodResolver} from '@hookform/resolvers/zod'
-
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "./ui/label";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 const forgetPassSchema = z.object({
-  email: z.email("Email không hợp lệ")
+  email: z.email("Email không hợp lệ"),
 });
 
-type ForgetPassFormValues = z.infer<typeof forgetPassSchema>
+type ForgetPassFormValues = z.infer<typeof forgetPassSchema>;
 
 export function ForgetpassForm({
   className,
   ...props
-}: React.ComponentProps<"div">){
-
-  const {register, handleSubmit, formState:{errors,isSubmitting}}= useForm<ForgetPassFormValues>({
-    resolver: zodResolver(forgetPassSchema)
+}: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm<ForgetPassFormValues>({
+    resolver: zodResolver(forgetPassSchema),
   });
 
   const onSubmit = async (data: ForgetPassFormValues) => {
-    //gọi backend để signup
-  }
-return (
-    <div className={cn("flex flex-col gap-6 w-full max-w-2xl mx-auto", className)} {...props}>
+    const API_URL = "http://localhost:5001/api/auth/forgot-password";
+    try {
+      const response = await axios.post(API_URL, data);
+      if (response.status === 200 && response.data.success) {
+        const successMessage =
+          "Yêu cầu khôi phục mật khẩu của bạn đã thành công, hãy kiểm tra hộp thư đến!";
+        alert(successMessage);
+        window.location.href = "/";
+      } else {
+        alert(response.data.message || "Lỗi không xác định");
+      }
+    } catch (error) {
+      console.error("Lỗi gì đó: ", error);
+      let errorMessage = "Đã xảy ra lỗi hệ thống";
+      if (axios.isAxiosError(error) && error.response) {
+        errorMessage =
+          error.response.data?.message || "Sai thông tin đăng nhập";
+      }
+      alert(errorMessage);
+    }
+  };
+  return (
+    <div
+      className={cn("flex flex-col gap-6 w-full max-w-2xl mx-auto", className)}
+      {...props}
+    >
       <Card className="overflow-hidden p-0 border-border ">
         <CardContent className="grid p-0">
           <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
@@ -37,35 +62,50 @@ return (
                   <img src="/logo.svg" alt="logo" />
                 </a>
                 <h1 className="text-2xl font-bold">Quên mật khẩu</h1>
-                <p className="text-muted-foreground text-balance">Nhập email để gửi OTP</p>
+                <p className="text-muted-foreground text-balance">
+                  Nhập email để gửi OTP
+                </p>
               </div>
               {/* email */}
-                <div className="flex flex-col gap-3">
-                  <Label htmlFor="email" className="block text-sm">Email</Label>
-                  <Input type="text" id="email" placeholder="k@gmail.com" {...register("email")}/>
-                  {errors.email && (
-                    <p className="text-destructive text-sm">
-                      {errors.email.message}
-                    </p>
-                  )}
-                </div>
+              <div className="flex flex-col gap-3">
+                <Label htmlFor="email" className="block text-sm">
+                  Email
+                </Label>
+                <Input
+                  type="text"
+                  id="email"
+                  placeholder="k@gmail.com"
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className="text-destructive text-sm">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
               {/* nút sen OTP*/}
-              <Button type="submit" className="w-full" disabled={isSubmitting}>
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={isSubmitting || !isValid}
+              >
                 Gửi mã OTP
               </Button>
 
               <div className="text-center text-sm">
-                Đã có tài khoản? {" "}
-                <a href="/signin" className="underline underline-offset-4">Đăng nhập</a>
+                Đã có tài khoản?{" "}
+                <a href="/signin" className="underline underline-offset-4">
+                  Gửi mã
+                </a>
               </div>
             </div>
           </form>
         </CardContent>
       </Card>
       <div className="text-xs text-balance px-6 text-center *:[a]:hover:text-primary text-muted-foreground *:[a]:underline *:[a]:underline-offset-4">
-        Bằng cách tiếp tục, bạn đồng ý với <a href="#">Điều khoản dịch vụ</a>{" "}
-        và <a href="#">Chính sách bảo mật của chúng tôi</a>.
+        Bằng cách tiếp tục, bạn đồng ý với <a href="#">Điều khoản dịch vụ</a> và{" "}
+        <a href="#">Chính sách bảo mật của chúng tôi</a>.
       </div>
     </div>
-  )
+  );
 }
